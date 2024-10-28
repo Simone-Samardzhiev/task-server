83dbfe0cffd10e17abc902291ca390af40ccad9b
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-28 18:01:19 +0200

    Add logging in repository.go

--- user/repository.go
+++ user/repository.go
@@ -1,20 +1,25 @@
 package user
 
-import "database/sql"
+import (
+	"database/sql"
+	"log"
+)
 
 // PostgresRepository is an implementation of Repository.
 type PostgresRepository struct {
-	database sql.DB
+	database *sql.DB
 }
 
 // CheckEmail will check in the database if the email is already in use.
 func (p *PostgresRepository) CheckEmail(email *string) (bool, error) {
 	query := "SELECT COUNT(id) FROM users WHERE email = $1"
 	row := p.database.QueryRow(query, *email)
-	var count int
+	log.Printf("Executing query in CheckEmail: %s | Parameters: %s", query, *email)
 
+	var count int
 	err := row.Scan(&count)
 	if err != nil {
+		log.Printf("Error in CheckEmail line 22 | Error: %v", err)
 		return false, err
 	}
 
@@ -25,7 +30,10 @@ func (p *PostgresRepository) CheckEmail(email *string) (bool, error) {
 func (p *PostgresRepository) AddUser(user *User) error {
 	query := "INSERT INTO users (id, email, password) VALUES ($1, $2, $3)"
 	_, err := p.database.Exec(query, user.Id, user.Email, user.Password)
+	log.Printf("Execiting query in AddUser: %s | Parameters: %s, %s, %s", query, user.Id.String(), user.Email, user.Password)
+
 	if err != nil {
+		log.Printf("Error in AddUser line 36 | Error: %v", err)
 		return err
 	}
 
@@ -36,10 +44,12 @@ func (p *PostgresRepository) AddUser(user *User) error {
 func (p *PostgresRepository) GetUserByEmail(email *string) (*User, error) {
 	query := "SELECT * FROM users WHERE email = $1"
 	row := p.database.QueryRow(query, *email)
-	var user User
+	log.Printf("Executing query in GetUserByEmail: %s | Parameters: %s", query, *email)
 
+	var user User
 	err := row.Scan(&user.Id, &user.Email, &user.Password)
 	if err != nil {
+		log.Printf("Error in GetUserByEmail line 52 | Error: %v", err)
 		return nil, err
 	}
 
