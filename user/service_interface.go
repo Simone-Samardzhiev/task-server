357544c8c25068036297f51902e7b5765f5ab8d1
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-29 08:34:36 +0200

    Add service_interface.go

---
+++ user/service_interface.go
@@ -0,0 +1,13 @@
+package user
+
+// Service defines the methods for a user service.
+type Service interface {
+	// Login will return a refresh token.
+	Login(*WithoutIdUser) (*string, error)
+
+	// Register will register the user.
+	Register(*WithoutIdUser) (*string, error)
+
+	// RefreshTokens will return a new refresh token and access token using the access token.
+	RefreshTokens(*string) (*TokenGroup, error)
+}
