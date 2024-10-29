6b7f62030f07c22b378f6cc782ea61ddc9dbf5b1
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-29 11:58:43 +0200

    Refactor repository_interface.go

--- user/repository_interface.go
+++ user/repository_interface.go
@@ -1,5 +1,7 @@
 package user
 
+import "github.com/google/uuid"
+
 // Repository that defines the methods for a user repository.
 type Repository interface {
 	// CheckEmail will check if the email is already in use.
@@ -10,4 +12,13 @@ type Repository interface {
 
 	// GetUserByEmail will return a user with a specific email.
 	GetUserByEmail(*string) (*User, error)
+
+	// AddToken will add a new token.
+	AddToken(*RefreshToken) error
+
+	// DeleteTokenById will delete a token with a specific id.
+	DeleteTokenById(uuid *uuid.UUID) error
+
+	// GetTokenById will get the token with a specific id.
+	GetTokenById(uuid *uuid.UUID) (*RefreshToken, error)
 }
