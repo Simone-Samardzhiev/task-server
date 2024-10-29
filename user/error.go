6d021586d6f2991e4eacf095cbcb2338b8f04704
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-29 12:43:32 +0200

    Add new error

--- user/error.go
+++ user/error.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrWrongCredentials = errors.New("wrong credentials")
 	ErrEmailInUse       = errors.New("email is already in use")
+	ErrInvalidToken     = errors.New("invalid token")
 )
