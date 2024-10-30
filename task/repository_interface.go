645d939e601a81588694184fe17eb2297829f822
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-30 17:03:17 +0200

    Add CheckPriority method in repository_interface.go

--- task/repository_interface.go
+++ task/repository_interface.go
@@ -5,13 +5,16 @@ import "github.com/google/uuid"
 // Repository defines methods for task repository.
 type Repository interface {
 	// GetTasks will get all task of a user.
-	GetTasks(uuid *uuid.UUID) ([]Task, error)
+	GetTasks(*uuid.UUID) ([]Task, error)
+
+	// CheckPriority will check if the priority us valid.
+	CheckPriority(*int64) (bool, error)
 
 	// AddTask will add a new task to a user.
 	AddTask(*Task, *uuid.UUID) error
 
 	// UpdateTask will update an existing task.
-	UpdateTask(*Task) (Task, error)
+	UpdateTask(*Task) error
 
 	// DeleteTask will delete a task with a specific id.
 	DeleteTask(*uuid.UUID) error
