f8d20a140cf96ec323d1f4f025b3463afe4ceb4a
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-30 13:08:43 +0200

    Add null_time.go

---
+++ task/null_time.go
@@ -0,0 +1,28 @@
+package task
+
+import (
+	"database/sql"
+	"encoding/json"
+)
+
+type NullTime struct {
+	sql.NullTime
+}
+
+func (t NullTime) MarshalJSON() ([]byte, error) {
+	if t.Valid {
+		return json.Marshal(t.Time)
+	} else {
+		return json.Marshal(nil)
+	}
+}
+
+func (t *NullTime) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		t.Valid = false
+		return nil
+	}
+
+	t.Valid = true
+	return json.Unmarshal(data, &t.Time)
+}
