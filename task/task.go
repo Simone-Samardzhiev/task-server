c3cf03bbdb1c671cc21015b9e582ed22568334b5
simonesamardzhiev <simonesamardzhiev@gmail.com>
2024-10-30 15:52:56 +0200

    Add NewTask in task.go

--- task/task.go
+++ task/task.go
@@ -1,14 +1,25 @@
 package task
 
 import (
+	"github.com/google/uuid"
 	"time"
 )
 
+// Task defines the data stored in a task.
 type Task struct {
+	Id            uuid.UUID `json:"id"`
 	Name          string    `json:"name"`
 	Description   string    `json:"description"`
 	Priority      int       `json:"priority"`
 	DueDate       time.Time `json:"dueDate"`
 	DateCompleted NullTime  `json:"dateCompleted"`
 	DateDeleted   NullTime  `json:"dateDeleted"`
+}
+
+// NewTask is a task that will be added.
+type NewTask struct {
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Priority    int       `json:"priority"`
+	DueDate     time.Time `json:"dueDate"`
 }
